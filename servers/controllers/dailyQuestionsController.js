bf43173aaf05bce2676c76e60ef9c8cc2f5f966c
mina <m1nna@naver.com>
2021-11-30 18:34:10 +0900

    mina

--- servers/controllers/dailyQuestionsController.js
+++ servers/controllers/dailyQuestionsController.js
@@ -1,12 +1,12 @@
 import { datas } from "../models/dailyQuestion";
 
 export const addDaily = async (req, res) => {
-  // const data = req.body.data[0];
-  // console.log(data);
+  const data = req.body.data[0];
+  console.log(data);
   let i = 0;
   while (i < 70) {
     const data = req.body.data[i];
-    await datas.create({ datas: [data] });
+    await datas.create({ datas: data });
     i += 0;
   }
   console.log("success");
