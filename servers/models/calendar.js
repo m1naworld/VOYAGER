597a8e73c733c6e54baef14ae3b6871c8b18f17b
mina <m1nna@naver.com>
2021-12-03 10:07:20 +0900

    mina

--- servers/models/calendar.js
+++ servers/models/calendar.js
@@ -6,35 +6,34 @@ moment.tz.setDefault("Asia/Seoul");
 
 const registerDate = moment().format("YYYY-MM-DD HH:mm:ss");
 
-const dailyQandASchema = new mongoose.Schema(
-  {
-    label: Number,
-    dailyQ: [String],
-    dailyA: [String],
-  },
-  { versionKey: false }
-);
-
-export const dailyQandA = mongoose.model("dailyQandA", dailyQandASchema);
-
-const dataSchema = new mongoose.Schema(
-  {
-    date: { type: String, default: registerDate },
-    colors: { type: [Number] },
-    diary: { type: String },
-    dailyQandA: { type: mongoose.SchemaType.ObjectId, ref: "dailyQandA" },
-  },
-  { versionKey: false }
-);
-
-export const data = mongoose.model("data", dataSchema);
-
 const calendarSchema = new mongoose.Schema(
   {
-    snsId: { type: String, required: true, unique: true },
-    data: { type: mongoose.SchemaType.ObjectId, ref: "data" },
+    // snsId: { type: String, required: true, unique: true },
+    data: {
+      type: Object,
+      date: { type: String, default: registerDate },
+      colors: { type: String },
+      diary: { type: String },
+      dailyA: {
+        type: Object,
+        // label: { type: mongoose.SchemaTypes.ObjectId, ref: "dailyquestion " },
+        label: Number,
+        data: [Object],
+      },
+    },
   },
   { versionKey: false }
 );
+
+calendarSchema.statics.findUser = function ({ snsId }) {
+  return this.findOne({ snsId });
+};
+
+calendarSchema.statics.register = function ({ dailyA }) {
+  const calendar = new this({ data: dailyA });
+  return calendar.save();
+};
+
+// calendarSchema.statics.findDate = function ({data.date})
 
 export const calendar = mongoose.model("calendar", calendarSchema);
