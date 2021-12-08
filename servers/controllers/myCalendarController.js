d21572e2ad8d0d914cbb6b834abbdf51ec4b1cf5
mina <m1nna@naver.com>
2021-12-08 12:45:43 +0900

    mina

---
+++ servers/controllers/myCalendarController.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+import { mycalendar } from "../models/myCalendar";
+import { mycolor } from "../models/myColor";
+import { mydaily } from "../models/myDaily";
+import { mydiary } from "../models/myDiary";
+
+export const addCalendar = async (req, res) => {
+  const accesstoken = req.cookies.Authorization;
+  const decoded = jwt.verify(accesstoken, process.env.JWT_SECRET);
+  const snsId = decoded.id;
+  console.log(snsId);
+  await mycolor.registerSnsId({ snsId });
+
+  await mydiary.registerSnsId({ snsId });
+  await mydaily.registerSnsId({ snsId });
+  console.log(" 왜안되냐 ");
+  let color = await mycolor.findOne({ snsId });
+  let diary = await mydiary.findOne({ snsId });
+  let daily = await mydaily.findOne({ snsId });
+  color = color._id;
+  diary = diary._id;
+  daily = daily._id;
+  const a = await mycalendar.registerData({ snsId, color, diary, daily });
+  console.log(a);
+};
