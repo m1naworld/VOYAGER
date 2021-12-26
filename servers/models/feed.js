2948977cbe9011b5591e4f8434bebecc54799427
mina <m1nna@naver.com>
2021-12-26 22:32:47 +0900

    mina

--- servers/models/feed.js
+++ servers/models/feed.js
@@ -9,6 +9,7 @@ const feedSchema = new mongoose.Schema(
     answer: Object,
     likeCount: { type: Number, default: 0 },
     user: [],
+    status: { type: Boolean, default: false },
   },
   { versionKey: false }
 );
