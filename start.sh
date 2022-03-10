29c8fac79ef50a4c6dd3e927a60285187fca9a98
root <root@ip-172-31-7-35.ap-northeast-2.compute.internal>
2022-03-10 13:05:00 +0000

    ubuntu

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 docker-compose down
+docker rm -f `docker ps -a -q`
 docker rmi `docker images -q`
 git pull
 docker-compose up --build -d --scale backend=3
-docker-compose logs -f
+#docker-compose logs -f
